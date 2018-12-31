35d3d974807d0609e4b43c4dd5c7f1e3de45ae84
David An <dong82@gmail.com>
2018-12-31 13:03:14 -0500

    Init github

---
+++ styletransfermodel.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import scipy.io
+import scipy.misc
+from PIL import Image
+from nst_utils import *
+import numpy as np
+import tensorflow as tf
+
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+matplotlib.pyplot.ion()
+
+
+
+def mainfunc():
